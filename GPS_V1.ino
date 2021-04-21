2ecbaa2136cc0dd5b521a3b0d3d44594bb7b6ab1
Zefy <45132484+ZefyP@users.noreply.github.com>
2021-04-21 17:53:49 +0100

    Create GPS_V1.ino

---
+++ GPS_V1.ino
@@ -0,0 +1,7 @@
+// this is the code branch for GPS location tracking to calculate the final recovery distance from the launchpad
+//
+//ATTENTION Jamiee: GPS traccking, application, main code
+//Program function:
+// Location
+// Distance from Launchpad
+// Estimated Altitude (Cedric/Seb)
