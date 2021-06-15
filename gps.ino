35a3b3c1526465b3d18ec7a3d9360d4e484c6090
XYBUS <79996610+XYBUS@users.noreply.github.com>
2021-06-15 19:02:06 +0200

    Add files via upload

---
+++ gps.ino
@@ -0,0 +1,41 @@
+// this is the code branch for GPS location tracking to calculate the final recovery distance from the launchpad
+//
+//ATTENTION Jamiee: GPS traccking, application, main code
+//Program function:
+// Location
+// Distance from Launchpad
+// Estimated Altitude (Cedric/Seb)
+
+#include <TinyGPS++.h>
+#include <AltSoftSerial.h>
+
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+AltSoftSerial ss(RXPin, TXPin);
+
+void setup(){
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+}
+
+void loop(){
+  // Achieving values for latitude, longitude and altitude.
+  while (ss.available() > 0){
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= "); 
+      Serial.println(gps.location.lat(), 6);  // Latitude in degrees
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6);  // Longitude in degrees
+      Serial.print("Altitude= ");
+      Serial.println(gps.altitude.meters()); // Altitude in meters
+
+      // NEED LAUMCH PAD COORDS TO CALCULATE DISTANCE TO ROCKET
+    }
+  }
+}
