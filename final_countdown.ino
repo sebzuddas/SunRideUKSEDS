446982f67f60d184a98909924a4ee1b4c697e291
Ash2772 <79996740+Ash2772@users.noreply.github.com>
2021-06-16 18:06:00 +0100

    Update final_countdown.ino
    Made it do the thing where it creates a new file every 30 seconds

--- final_countdown.ino
+++ final_countdown.ino
@@ -17,6 +17,7 @@ char fileName[] = baseName "00.txt";
 int sensorPin = A0;
 
 unsigned long t;
+unsigned long Previous_time = 0;
  
 void setup() {
   Serial.begin(9600);
@@ -88,34 +89,31 @@ void loop() {
   int gasReading = analogRead(sensorPin); //gas sensor reading
 
 
-t = millis();
-for (int i=0;i<30;i++){
-   while (SD.exists(fileName)) {
-      if (fileName[baseNameSize + 1] != '9') {
-        fileName[baseNameSize + 1]++;
-      } else if (fileName[baseNameSize] != '9') {
-        fileName[baseNameSize + 1] = '0';
-        fileName[baseNameSize]++;
-      } else {
-        Serial.println("Can't create file name");
-        return;
-      }
+  t = millis();
+  if (millis() - Previous_time >= 30000) {
+    Previous_time = millis();
+    while (SD.exists(fileName)) {
+    if (fileName[baseNameSize + 1] != '9') {
+      fileName[baseNameSize + 1]++;
+    } else if (fileName[baseNameSize] != '9') {
+      fileName[baseNameSize + 1] = '0';
+      fileName[baseNameSize]++;
+    } else {
+      Serial.println("Can't create file name");
+      return;
     }
-    while(t<){
-
-    data += String(t) + ", " + String(temperature) + ", " + String(humidity)+ ", " + String(pressure)+", " + String(gasSensor);
-    File dataFile = SD.open(fileName, FILE_WRITE);
-    if (dataFile) {
-      dataFile.println(data);
-      dataFile.close();
-    }
-    else {
-      Serial.print("Error opening ");
-      Serial.println(fileName);
-    }
-    delay(10); // sampling rate BIATCH 
   }
-}
+  }
 
-  i++;
+  data += String(t) + ", " + String(temperature) + ", " + String(humidity)+ ", " + String(pressure)+", " + String(gasSensor);
+  File dataFile = SD.open(fileName, FILE_WRITE);
+  if (dataFile) {
+    dataFile.println(data);
+    dataFile.close();
+  }
+  else {
+    Serial.print("Error opening ");
+    Serial.println(fileName);
+  }
+  delay(10); // sampling rate BIATCH 
 }
